5f341cc97e8fa05caec5aaa762bd53cc3ac511d9
antoniojosue6171-arch <antoniojosue6171@gmail.com>
2026-04-09 22:32:37 -0600

    Add src/voteStore.ts

---
+++ src/voteStore.ts
@@ -0,0 +1,13 @@
+export const activeVote: {
+  messageId: string | null;
+  channelId: string | null;
+  civilesMin: number | null;
+  moderadoresMin: number | null;
+  initiator: string | null;
+} = {
+  messageId: null,
+  channelId: null,
+  civilesMin: null,
+  moderadoresMin: null,
+  initiator: null,
+};
